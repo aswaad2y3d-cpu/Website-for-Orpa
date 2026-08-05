5b1fc6dcccf4fd276445c137b7f08caf092eec00
aswaad2y3d-cpu <aswaad2y3d@gmail.com>
2026-08-05 23:57:24 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,57 +1,160 @@
 // Elements
+
 const envelope = document.getElementById("envelope-container");
 const letter = document.getElementById("letter-container");
+
 const noBtn = document.querySelector(".no-btn");
-const yesBtn = document.querySelector(".btn[alt='Yes']");
+const yesBtn = document.querySelector(".yes-btn");
 
 const title = document.getElementById("letter-title");
 const catImg = document.getElementById("letter-cat");
 const buttons = document.getElementById("letter-buttons");
 const finalText = document.getElementById("final-text");
 
-// Click Envelope
+
+// Open Envelope
 
 envelope.addEventListener("click", () => {
+
     envelope.style.display = "none";
     letter.style.display = "flex";
 
-    setTimeout( () => {
-        document.querySelector(".letter-window").classList.add("open");
+
+    setTimeout(() => {
+
+        document
+        .querySelector(".letter-window")
+        .classList.add("open");
+
     },50);
+
+
 });
 
 
-// Logic to make YES btn to grow
+
+
+// NO button runs away
+
+noBtn.addEventListener("mouseover", () => {
+
+
+    const x = Math.random() * 300 - 150;
+    const y = Math.random() * 200 - 100;
+
+
+    noBtn.style.transform =
+    `translate(${x}px,${y}px)`;
+
+});
+
+
+
+
+
+// YES button
 
 let yesScale = 1;
 
-yesBtn.style.position = "relative"
-yesBtn.style.transformOrigin = "center center";
-yesBtn.style.transition = "transform 0.3s ease";
 
-noBtn.addEventListener("click", () => {
-    yesScale += 2;
+noBtn.addEventListener("click",()=>{
 
-    if (yesBtn.style.position !== "fixed") {
-        yesBtn.style.position = "fixed";
-        yesBtn.style.top = "50%";
-        yesBtn.style.left = "50%";
-        yesBtn.style.transform = `translate(-50%, -50%) scale(${yesScale})`;
-    }else{
-       yesBtn.style.transform = `translate(-50%, -50%) scale(${yesScale})`;
-   }
+    yesScale +=0.3;
+
+    yesBtn.style.transform =
+    `scale(${yesScale})`;
+
 });
 
-// YES is clicked
 
-yesBtn.addEventListener("click", () => {
-    title.textContent = "Yippeeee!! I luv You";
 
-    catImg.src = "cat_dance.gif";
 
-    document.querySelector(".letter-window").classList.add("final");
+// YES CLICK
 
-    buttons.style.display = "none";
+yesBtn.addEventListener("click",()=>{
 
-    finalText.style.display = "block";
+
+    title.textContent =
+    "Yippeeee!! I knew you would say yes ❤️";
+
+
+    catImg.src="cat_dance.gif";
+
+
+    buttons.style.display="none";
+
+
+    finalText.style.display="block";
+
+
+    document
+    .querySelector(".letter-window")
+    .classList.add("final");
+
+
+    createHearts();
+
+
+
 });
+
+
+
+
+// Heart Explosion
+
+function createHearts(){
+
+
+    for(let i=0;i<80;i++){
+
+
+        let heart=document.createElement("div");
+
+        heart.innerHTML="❤️";
+
+
+        heart.style.position="fixed";
+
+        heart.style.left="50%";
+        heart.style.top="50%";
+
+
+        heart.style.fontSize=
+        Math.random()*30+15+"px";
+
+
+        heart.style.transition="3s";
+
+
+        document.body.appendChild(heart);
+
+
+
+        setTimeout(()=>{
+
+
+            heart.style.transform=
+            `translate(
+            ${Math.random()*800-400}px,
+            ${Math.random()*800-400}px
+            ) rotate(360deg)`;
+
+            heart.style.opacity=0;
+
+
+        },50);
+
+
+
+        setTimeout(()=>{
+
+            heart.remove();
+
+        },3000);
+
+
+    }
+
+
+}
